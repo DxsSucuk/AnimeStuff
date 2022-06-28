978e233dd81189a7c18b753e2ff7f5474840918a
DxsSucuk <dxssucuk@hotmail.com>
2022-06-28 19:40:19 +0200

    Initial commit

---
+++ src/main/java/de/presti/animestuff/AnimeStuff.java
@@ -0,0 +1,17 @@
+package de.presti.animestuff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnimeStuff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
