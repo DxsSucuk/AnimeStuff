4fcf0ca1f7637964dbb95e579699a7aa384e7859
Enton <antontutass@web.de>
2022-06-29 11:40:26 +0200

    * Updated.

--- src/main/java/de/presti/animestuff/utils/SchematicUtil.java
+++ src/main/java/de/presti/animestuff/utils/SchematicUtil.java
@@ -16,6 +16,7 @@ import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.session.PasteBuilder;
 import de.presti.animestuff.AnimeStuff;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.io.File;
@@ -24,51 +25,60 @@ import java.io.IOException;
 
 public class SchematicUtil {
 
-    public static void loadSchematic(String path, Player player) {
-        File schematic = new File("plugins/AnimeStuff/", path);
+    public static void loadAndPasteSchematic(String path, Player player) { loadAndPasteSchematic(path, player, player.getLocation()); }
+    public static void loadAndPasteSchematic(String path, Player player, Location location) {
+        Clipboard clipboard = loadSchematic(path);
+        pasteSchematic(clipboard, player, location);
+    }
 
-        if (!schematic.exists() || !schematic.isFile()) {
-            AnimeStuff.getInstance().getLogger().warning("Schematic file not found: " + path);
-            return;
-        }
-
+    public static void pasteSchematic(Clipboard clipboard, Player player, Location location) {
         if (Bukkit.getPluginManager().getPlugin("WorldEdit") instanceof WorldEditPlugin worldEditPlugin) {
-            try (EditSession editSession = worldEditPlugin.getWorldEdit().newEditSession(BukkitAdapter.adapt(player.getWorld()))) {                Clipboard clipboard;
-
-                ClipboardFormat format = ClipboardFormats.findByFile(schematic);
-
-                if (format == null) {
-                    AnimeStuff.getInstance().getLogger().warning("Schematic file format not supported: " + path);
-                    return;
-                }
-
-                try (ClipboardReader reader = format.getReader(new FileInputStream(schematic))) {
-                    clipboard = reader.read();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                if (clipboard == null) {
-                    AnimeStuff.getInstance().getLogger().warning("Could not load schematic: " + path);
-                    return;
-                }
-
+            try (EditSession editSession = worldEditPlugin.getWorldEdit().newEditSession(BukkitAdapter.adapt(player.getWorld()))) {
                 ClipboardHolder clipboardHolder = new ClipboardHolder(clipboard);
 
                 PasteBuilder pasteBuilder = clipboardHolder
                         .createPaste(editSession)
-                        .to(BlockVector3.at(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ()));
+                        .to(BlockVector3.at(location.getX(), location.getY(), location.getZ()))
+                        .ignoreAirBlocks(false);
 
                 Operation operation = pasteBuilder.build();
-
                 Operations.complete(operation);
 
                 worldEditPlugin.getSession(player).remember(editSession);
             } catch (Exception exception) {
-                AnimeStuff.getInstance().getLogger().warning("Could not load schematic: " + path);
+                AnimeStuff.getInstance().getLogger().warning("Failure loading a schematic.");
                 exception.printStackTrace();
             }
         }
+    }
+
+    public static Clipboard loadSchematic(String path) {
+        File schematic = new File("plugins/AnimeStuff/", path);
+
+        if (!schematic.exists() || !schematic.isFile()) {
+            AnimeStuff.getInstance().getLogger().warning("Schematic file not found: " + path);
+            return null;
+        }
+
+        Clipboard clipboard;
+        ClipboardFormat format = ClipboardFormats.findByFile(schematic);
+
+        if (format == null) {
+            AnimeStuff.getInstance().getLogger().warning("Schematic file format not supported: " + path);
+            return null;
+        }
+
+        try (ClipboardReader reader = format.getReader(new FileInputStream(schematic))) {
+            clipboard = reader.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (clipboard == null) {
+            AnimeStuff.getInstance().getLogger().warning("Could not load schematic: " + path);
+            return null;
+        }
+        return clipboard;
     }
 
 }
